470c0e1bedf9fb580d20de8bbccdd9c9003ef9c1
Jesus Mira Aldao <jesus.mira1@hotmail.com>
2020-03-28 18:49:47 +0100

    add a second panel

--- covapp/routes.py
+++ covapp/routes.py
@@ -1,4 +1,4 @@
-from covapp import app
+from myapp import app
 import json, plotly
 from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
@@ -7,7 +7,22 @@ from wrangling_scripts.wrangle_data import return_figures
 @app.route('/index')
 def index():
 
-    figures = return_figures()
+    figures = return_figures(panel = 1)
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html',
+                           ids=ids,
+                           figuresJSON=figuresJSON)
+
+@app.route('/index_panel_2')
+def index_panel_2():
+
+    figures = return_figures(panel = 2)
 
     # plot ids for the html id tag
     ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
